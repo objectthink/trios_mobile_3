eaddaf3430817353e450836d793b02eeb049fea8
stephen eshelman <objectthink@comcast.net>
2016-02-21 12:38:27 -0500

    update send command callback create event and response dictionary outside of call to dispatch request make subCommandId a property to make it available to swift derived classes initial trios_mobile_3 upload

---
+++ InstrumentCell.swift
@@ -0,0 +1,249 @@
+//
+//  InstrumentCell.swift
+//  TRIOS_MOBILE_3
+//
+//  Created by stephen eshelman on 2/20/16.
+//  Copyright © 2016 objectthink.com. All rights reserved.
+//
+
+import UIKit
+
+class GetSerialNumber:MercuryGet
+{
+   override init()
+   {
+      super.init()
+      self.subCommandId = 0x00000002
+   }
+}
+
+class GetName:MercuryGet
+{
+   override init()
+   {
+      super.init()
+      self.subCommandId = 0x00000003
+   }
+}
+
+class GetLocation:MercuryGet
+{
+   override init()
+   {
+      super.init()
+      self.subCommandId = 0x00000004
+   }
+}
+
+class InstrumentCell: UICollectionViewCell, MercuryInstrumentDelegate
+{
+    
+   @IBOutlet var _instrumentName: UILabel!
+   @IBOutlet var _statusLabel: UILabel!
+   @IBOutlet var _name: UILabel!
+   @IBOutlet var _serialNumber: UILabel!
+   @IBOutlet var _location: UILabel!
+   
+   var _instrument:MercuryInstrument!
+   
+   var instrument:MercuryInstrument!
+   {
+      get{ return _instrument}
+      set
+      {
+         _instrument = newValue
+         _instrument.addDelegate(self)
+         
+         _instrument.sendCommand(MercuryGetProcedureStatusCommand())
+         { (r) -> Void in
+         
+            self._statusLabel.text = "Some status"
+            
+            let response = MercuryProcedureStatusResponse(message: NSData(data: r.bytes))
+            
+            print(response.endStatus)
+            print(response.runStatus)
+            print(response.currentSegmentId)
+            
+            let runStatus = response.runStatus.rawValue
+            dispatch_async(dispatch_get_main_queue(),
+               { () -> Void in
+                  switch(runStatus)
+                  {
+                  case  0:
+                     self._statusLabel.text = "Status: Idle"
+                  case 1:
+                     self._statusLabel.text = "Status: PreTest"
+                  case 2:
+                     self._statusLabel.text = "Status: Test"
+                  case 3:
+                     self._statusLabel.text = "Status: PostTest"
+                  default:
+                     self._statusLabel.text = "AAAAAA"
+                  }
+                  
+            })
+
+            print(self._statusLabel.text)
+         }
+         
+         _instrument.sendCommand(GetSerialNumber())
+         {(response)-> Void in
+            
+            if let str = NSString(data: response.bytes, encoding: NSUTF8StringEncoding) as? String
+            {
+               print(str)
+               
+               dispatch_async(dispatch_get_main_queue(),
+               { () -> Void in
+                  self._serialNumber.text = str
+                  print("SERIAL")
+                  print(str)
+               })
+            }
+            else
+            {
+               print("not a valid UTF-8 sequence")
+            }
+         }
+         
+         _instrument.sendCommand(GetName())
+         { (response) -> Void in
+            if let str = NSString(data: response.bytes, encoding: NSUTF8StringEncoding) as? String
+            {
+               print(str)
+               
+               dispatch_async(dispatch_get_main_queue(),
+               { () -> Void in
+                     self._name.text = self.mercuryStringFixup(str)
+                     print("NAME")
+                     print(str)
+               })
+            }
+            else
+            {
+               print("not a valid UTF-8 sequence")
+            }
+         }
+         
+         _instrument.sendCommand(GetLocation())
+            { (response) -> Void in
+               if let str = NSString(data: response.bytes, encoding: NSASCIIStringEncoding) as? String
+               {
+                  print(str)
+                  
+                  dispatch_async(dispatch_get_main_queue(),
+                     { () -> Void in
+                        //let newStr =
+                        //str.stringByTrimmingCharactersInSet(NSCharacterSet(charactersInString: "\0"))
+                        
+                        self._location.text = self.mercuryStringFixup(str)
+                        
+                        print("LOCATION")
+                        print(str)
+                  })
+               }
+               else
+               {
+                  print("not a valid UTF-8 sequence")
+               }
+         }
+      }
+   }
+
+   func connected()
+   {
+      print("connected")
+   }
+
+   func accept(access: MercuryAccess)
+   {
+      print("accept")
+   }
+   
+   var _statCount:Int = 0
+   func stat(message: NSData!, withSubcommand subcommand: uint)
+   {
+      _statCount++
+      
+//      if _statCount >= 100
+//      {
+//         print("stat 100")
+//         _statCount = 0
+//      }
+      
+      //print(subcommand)
+      
+      if subcommand == ProcedureStatus.rawValue
+      {
+         let response = MercuryProcedureStatus(message: message)
+         print(response.endStatus)
+         print(response.runStatus)
+         print(response.currentSegmentId)
+         
+         let runStatus = response.runStatus.rawValue
+         dispatch_async(dispatch_get_main_queue(),
+            { () -> Void in
+               switch(runStatus)
+               {
+               case  0:
+                  self._statusLabel.text = "Status: Idle"
+               case 1:
+                  self._statusLabel.text = "Status: PreTest"
+               case 2:
+                  self._statusLabel.text = "Status: Test"
+               case 3:
+                  self._statusLabel.text = "Status: PostTest"
+               default:
+                  self._statusLabel.text = ""
+               }
+               
+         })
+         
+         print(self._statusLabel.text)
+      }
+   }
+   
+   func response(
+      message: NSData!,
+      withSequenceNumber sequenceNumber: uint,
+      subcommand: uint,
+      status: uint)
+   {
+      print("response")
+      
+      if (subcommand == 9)
+      {
+         let response = MercuryProcedureStatusResponse(message: message)
+         print(response.endStatus)
+         print(response.runStatus)
+         print(response.currentSegmentId)
+      }
+   }
+   
+   func ackWithSequenceNumber(sequencenumber: uint)
+   {
+      print("ack")
+   }
+   
+   func nakWithSequenceNumber(sequencenumber: uint, andError errorcode: uint)
+   {
+      print("nak")
+   }
+   
+   func mercuryStringFixup(s:String)->String
+   {
+      var sOut:String = String()
+      
+      for character in s.characters
+      {
+         if character != "\0"
+         {
+            sOut.append(character)
+         }
+      }
+      
+      return sOut
+   }
+   
+}
---
+++ TRIOS_MOBILE_3/InstrumentCollectionViewController.swift
@@ -0,0 +1,162 @@
+//
+//  InstrumentCollectionViewController.swift
+//  TRIOS_MOBILE_3
+//
+//  Created by stephen eshelman on 2/20/16.
+//  Copyright © 2016 objectthink.com. All rights reserved.
+//
+
+import UIKit
+
+class InstrumentCollectionViewController: UICollectionViewController
+{
+   var _instruments: [MercuryInstrument] = [];
+   
+   private let reuseIdentifier = "InstrumentCell"
+   private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+   
+   override func viewDidLoad()
+   {
+      super.viewDidLoad()
+      
+      // Uncomment the following line to preserve selection between presentations
+      // self.clearsSelectionOnViewWillAppear = false
+      
+      // Register cell classes
+      //self.collectionView!.registerClass(
+      //   UICollectionViewCell.self,
+      //   forCellWithReuseIdentifier: reuseIdentifier)
+      
+      // Do any additional setup after loading the view.
+   }
+   
+   @IBAction func addInstrument(sender: AnyObject)
+   {
+      //1. Create the alert controller.
+      let alert = UIAlertController(
+         title: "TA Instruments",
+         message: "Enter instrument IP address.",
+         preferredStyle: .Alert)
+      
+      //2. Add the text field. You can configure it however you need.
+      alert.addTextFieldWithConfigurationHandler(
+      { (textField) -> Void in
+         textField.text = ""
+      })
+      
+      //3. Grab the value from the text field, and print it when the user clicks OK.
+      alert.addAction(
+         UIAlertAction(
+            title: "OK",
+            style: .Default,
+            handler:
+            { (action) -> Void in
+               let textField = alert.textFields![0] as UITextField
+               
+               let instrument:MercuryInstrument = MercuryInstrument()
+               
+               instrument.connectToHost(textField.text, andPort: 8080)
+               
+               instrument.loginWithUsername(
+                  "NEW_AND_IMPROVED_IPAD",
+                  machineName: "MAC",
+                  ipAddress: "10.10.0.0",
+                  access: 1000)
+               
+               self._instruments.append(instrument)
+               
+               self.collectionView?.reloadData()
+            }))
+      
+      alert.addAction(
+         UIAlertAction(
+            title: "Cancel",
+            style: .Default,
+            handler:
+            { (action) -> Void in
+            }))
+
+      // 4. Present the alert.
+      self.presentViewController(alert, animated: true, completion: nil)
+   }
+   
+   override func didReceiveMemoryWarning()
+   {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+   
+   /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+   // Get the new view controller using [segue destinationViewController].
+   // Pass the selected object to the new view controller.
+   }
+   */
+   
+   // MARK: UICollectionViewDataSource
+   
+   override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int
+   {
+      // #warning Incomplete implementation, return the number of sections
+      return 1
+   }
+   
+   
+   override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+   {
+      // #warning Incomplete implementation, return the number of items
+      return _instruments.count
+   }
+   
+   override func collectionView(
+      collectionView: UICollectionView,
+      cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell
+   {
+      let cell:InstrumentCell = collectionView.dequeueReusableCellWithReuseIdentifier(
+         reuseIdentifier,
+         forIndexPath: indexPath) as! InstrumentCell
+      
+      cell.instrument = _instruments[indexPath.row]
+      
+      return cell
+   }
+   
+   // MARK: UICollectionViewDelegate
+   
+   /*
+   // Uncomment this method to specify if the specified item should be highlighted during tracking
+   override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool
+   {
+   return true
+   }
+   */
+   
+   /*
+   // Uncomment this method to specify if the specified item should be selected
+   override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool
+   {
+   return true
+   }
+   */
+   
+   /*
+   // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+   override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool
+   {
+   return false
+   }
+   
+   override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool
+   {
+   return false
+   }
+   
+   override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?)
+   {
+   
+   }
+   */
+}
